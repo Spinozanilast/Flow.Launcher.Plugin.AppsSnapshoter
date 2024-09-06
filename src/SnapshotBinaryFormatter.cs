3a28d8c65b86f7fc6670d2ea6131f8cbce7e8d7d
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2024-09-06 11:08:37 +0300

    move formatter into separate file

---
+++ src/SnapshotBinaryFormatter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Flow.Launcher.Plugin.SnapshotApps.Models;
+using ProtoBuf;
+
+namespace Flow.Launcher.Plugin.SnapshotApps;
+
+public class SnapshotBinaryFormatter
+{
+    public static void SerializeSnapshot(FileStream stream, Snapshot snapshot)
+    {
+        using (stream)
+        {
+            Serializer.Serialize(stream, snapshot);
+        }
+    }
+
+    public static Snapshot DeserializeSnapshot(FileStream stream)
+    {
+        Snapshot snapshot;
+        using (stream)
+        {
+            snapshot = Serializer.Deserialize<Snapshot>(stream);
+        }
+
+        if (snapshot is null)
+        {
+            throw new NullReferenceException(
+                "Snapshot file was empty or not correct format.");
+        }
+
+        return snapshot;
+    }
+}
